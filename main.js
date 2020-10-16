ab2e084bc464be91a3a3fec8949daf6fe8096e53
aggscalco <72931646+aggscalco@users.noreply.github.com>
2020-10-16 20:29:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,8 @@ client.on('message', Message =>{
         client.commands.get('yt').execute(Message, args);
     }else if (Message.content === `${prefix}online`) {
         Message.channel.send(`Total Members: ${Message.guild.memberCount}`);
-    }
+    }else if (command == 'help'){
+        client.commands.get('help').execute(Message, args);
 });
 
 client.login(process.env.TOKEN);
