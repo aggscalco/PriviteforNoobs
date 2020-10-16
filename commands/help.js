1560564b90855b852713ca75f069bbf52ec59574
aggscalco <72931646+aggscalco@users.noreply.github.com>
2020-10-16 16:00:12 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,9 +2,7 @@ module.exports = {
     name: 'help',
     description: "this is a help command!" ,
     execute(Message, args){
-        Message.channel.send('```yaml');
-        Message.channel.send('-giveaway its beta, -yt or -youtube is for the channel');
-        Message.channel.send('```');
+        Message.channel.send('```yaml-giveaway its beta, -yt or -youtube is for the channel```');
    }
 
 
