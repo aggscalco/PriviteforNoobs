50c631c0ae6f9026a9ef776e4e240e115032d430
aggscalco <72931646+aggscalco@users.noreply.github.com>
2020-10-16 15:44:08 +0300

    Create giveaway.js

---
+++ commands/giveaway.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'giveaway',
+    description: "this is a giveaway command!" ,
+    execute(Message, args){
+        Message.channel.send('uhh coming soon lol');
+   }
+
+
+}
