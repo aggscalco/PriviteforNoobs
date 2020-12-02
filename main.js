ef3e01e203d4d66b71a6e97efc051accbd9b9672
aggscalco <72931646+aggscalco@users.noreply.github.com>
2020-12-02 21:26:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ client.once('ready', () => {
 
 client.on('guildMemberAdd', member => {
      
-    const channel = member.guild.channels.find(channel => channel.name === "join-leave-activity")
+    const channel = member.guild.channels.find(channel => channel.name === "704353497663012944")
     if(!channel) return;
 
     channel.send('Welcome ${member} to our server please read the rules!')
